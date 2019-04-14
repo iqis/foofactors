81f0d0ea7748c291c7cc4b715adbcc1fce0b3735
Siqi Zhang <iqis.gnahz@gmail.com>
2019-04-14 15:33:15 -0500

    add test fbind()

---
+++ R/fbind.R
@@ -0,0 +1,13 @@
+#' Combine two factors, including levels
+#'
+#' @param a `factor`
+#' @param b `factor`
+#'
+#' @return `factor`, combined `a` and `b` including levels
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species, Puromycin$state)
+fbind <- function(a, b){
+        forcats::fct_c(a, b)
+}
